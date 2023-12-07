16824aa4b1ffef1455e77d6c90a360973c3aa99b
KEBE <cheikh.kebe@sogeti.com>
2023-12-07 10:56:11 +0100

    Initial commit

---
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -0,0 +1,67 @@
+package com.pluralsight.calcengine;
+
+public class MathEquation {
+    private double leftVal;
+    private double rightVal;
+    private char opCode;
+    private double result;
+
+    private static int numberOfCalculations;
+    private static double sumOfResults;
+    void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0 ? leftVal / rightVal : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+
+        numberOfCalculations++;
+        sumOfResults += result;
+    }
+
+    public static double getAverageResult(){
+        return sumOfResults/numberOfCalculations;
+    }
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+}
