6b454615197032fec407dcbd1d35e3890511cd30
chkebe <cheikh.kebe.01@gmail.com>
2023-12-12 10:38:55 +0100

    [CalcEngine](inheritance) update each class with constructors and abstract calculate constructor

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -7,8 +7,40 @@ public class Main {
 
     public static void main(String[] args) {
         performCalculations();
+
+        Divider divider = new Divider();
+        doCalculation(divider, 100.0d, 50.0d);
+
+        Adder adder = new Adder();
+        doCalculation(adder, 25.0d, 92.0d);
+
+        performMoreCalculation();
     }
 
+    private static void performMoreCalculation() {
+        CalculateBase[] calculations = {
+                new Divider(100.0d, 50.0d),
+                new Adder(25.0d, 92.0d),
+                new Subtracter(225.0d, 17.0d),
+                new Multiplier(11.0d, 3.0d)
+        };
+
+        System.out.println();
+        System.out.println("Array Calculalion");
+
+        for(CalculateBase calculation : calculations) {
+            calculation.calculate();
+            System.out.println("result = " + calculation.getResult());
+        }
+    }
+
+    static void doCalculation(CalculateBase calculation, double leftVal, double rightVal){
+        calculation.setLeftVal(leftVal);
+        calculation.setRightVal(rightVal);
+        calculation.calculate();
+
+        System.out.println("Calculation result = " + calculation.getResult());
+    }
     static void performCalculations() {
         MathEquation[] equations = new MathEquation[4];
         equations[0] = create(100.0d, 50.0d, 'd');
